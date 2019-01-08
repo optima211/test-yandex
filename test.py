7b4c0c578128fb750ccfcf0a4e108d7de83bd4f5
optima211 <nedmk@mail.ru>
2019-01-08 09:51:39 +0700

    add test <yandex-image>

--- test.py
+++ test.py
@@ -31,11 +31,46 @@ class GoogleSearch(unittest.TestCase):
         for title in titles:
             assert "tensor.ru" in title.text.lower()
             time.sleep(4)
-            # input_field = driver.find_element_by_link_text("Тензор — IT - компания").click()
 
-            # input_field = driver.find_element_by_partial_link_text("tensor.ru").click()
-            # input_field.send_keys(Keys.ENTER)
-            # time.sleep(4)
+    def test_02(self):
+        driver = self.driver
+        print("1")
+        driver.find_element_by_link_text("Картинки").click()
+        print("2")
+        time.sleep(2)
+        st = driver.current_url
+        # print(st)
+        if st == "https://yandex.ru/images/":
+            print("step")
+            input_field = driver.find_element_by_class_name('cl-teaser__link').click()
+            time.sleep(2)
+            img1 = driver.current_url
+            # print(img1)
+            input_fields = driver.find_element_by_class_name('layout__nav__right').click()
+            img2 = driver.current_url
+            time.sleep(2)
+            # print(img2)
+            if img1!=img2:
+                input_fields = driver.find_element_by_class_name('layout__nav__left').click()
+                time.sleep(2)
+                if img1==driver.current_url:
+                    print("step2")
+                else:
+                    print("first image is modify!")
+            else:
+                print("image is not modify!")
+        else:
+            print("page image is not correct!")
+
+
+
+
+
+
+
+
+
+
 
 
     def tearDown(self):
